adc2be07c1b4ac0612afa6fd045e7832e9995432
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-08-28 16:00:14 +0300

    Add solution to exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,19 +4,19 @@ const Header = ({ course }) => {
   )
 }
 
+const Part = ({ part, exercises }) => {
+  return (
+    <p>{part} {exercises}</p>
+  )
+}
+
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
   )
 }
 
