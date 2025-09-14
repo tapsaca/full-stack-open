37710d6bc035191545461fd603697d3d9624f431
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-09-14 13:58:43 +0300

    Add solution to exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,6 +8,10 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     const newPerson = { name: newName }
     setPersons(persons.concat(newPerson))
     setNewName('')
