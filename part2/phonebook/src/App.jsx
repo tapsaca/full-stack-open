7b6c6edf997ed41f56adc2e3e25ed8096345ab71
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-09-19 21:06:17 +0300

    Add solution to exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -25,9 +25,13 @@ const App = () => {
       return
     }
     const newPerson = { name: newName, number: newNumber }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then((response) => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   const handleFilterChange = (event) => {
