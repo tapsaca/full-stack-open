db5e117a1a50e2d289a34efdc5c434a1a86f0a3d
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-09-30 18:23:41 +0300

    Add solution to exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -33,6 +33,15 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find((person) => person.id === id)
+  if (!person) {
+    return response.status(404).end()
+  }
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
