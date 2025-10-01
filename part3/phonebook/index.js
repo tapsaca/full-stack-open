63142a748ae9ae1313ade1c584cba3b741adb092
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-10-01 19:16:26 +0300

    Add solution to exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const persons = [
+let persons = [
   {
     id: '1',
     name: 'Arto Hellas',
@@ -40,6 +40,12 @@ app.get('/api/persons/:id', (request, response) => {
     return response.status(404).end()
   }
   response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter((person) => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
