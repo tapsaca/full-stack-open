61a2b10ac9318093bb7d254ea6534d8dd4d25d4c
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-10-12 15:24:19 +0300

    Add solution to exercise 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -49,9 +49,9 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter((person) => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id).then((result) => {
+    response.status(204).end()
+  })
 })
 
 const PORT = process.env.PORT
