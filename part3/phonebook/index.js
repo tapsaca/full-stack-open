f94ee38d1cb9746270878f21cd245e19e320af26
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-10-13 17:13:23 +0300

    Add solution to exercise 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -18,9 +18,11 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons)
-  })
+  Person.find({})
+    .then((persons) => {
+      response.json(persons)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -45,16 +47,30 @@ app.post('/api/persons', (request, response) => {
     })
   }
   const person = new Person({ name: body.name, number: body.number })
-  person.save().then((savedPerson) => response.json(savedPerson))
+  person.save()
+    .then((savedPerson) => response.json(savedPerson))
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then((result) => {
-    response.status(204).end()
-  })
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
