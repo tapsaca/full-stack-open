aa5ec4d659c44fc28281a01f34fbf7df20330165
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-10-14 23:01:35 +0300

    Add solution to exercise 3.17

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -52,6 +52,23 @@ app.post('/api/persons', (request, response) => {
     .catch((error) => next(error))
 })
 
+app.put('/api/persons/:id', (request, response) => {
+  const { name, number } = request.body
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      person.name = name
+      person.number = number
+      return person.save()
+        .then((updatedPerson) => {
+          response.json(updatedPerson)
+        })
+    })
+    .catch((error) => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
