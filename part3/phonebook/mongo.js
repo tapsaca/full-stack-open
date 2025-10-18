37943c8dae92219ce2c6f8ea91278cd8fbf98c00
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-10-18 19:29:06 +0300

    Add solution to exercise 3.22

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -31,7 +31,7 @@ if (process.argv.length === 3) {
     name: process.argv[3],
     number: process.argv[4]
   })
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(`added ${process.argv[3]} ${process.argv[4]} to phonebook`)
     mongoose.connection.close()
   })
