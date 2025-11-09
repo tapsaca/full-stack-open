39477b3bfad8ac4fa7164e6036e486deee832b98
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-11-09 17:04:48 +0200

    Add solution to exercise 4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -12,6 +12,8 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 
+app.use(middleware.tokenExtractor)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,3 +1,11 @@
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -16,6 +24,7 @@ const errorHandler = (error, request, response, next) => {
 }
 
 module.exports = {
+  tokenExtractor,
   unknownEndpoint,
   errorHandler
 }
