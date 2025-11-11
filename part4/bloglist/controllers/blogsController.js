b6f9fac39502eac765f939b0f55cbcff6d533fa8
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-11-11 15:19:03 +0200

    Add solution to exercise 4.22

--- part4/bloglist/controllers/blogsController.js
+++ part4/bloglist/controllers/blogsController.js
@@ -1,23 +1,18 @@
 const blogsRouter = require('express').Router()
-const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
-const User = require('../models/user')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   return response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const blog = new Blog(request.body)
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
+  if (!request.user) {
     return response.status(401).json({ error: 'token invalid' })
   }
-  const user = await User.findById(decodedToken.id)
-  if (!user) {
-    return response.status(400).json({ error: 'user missing or not valid' })
-  }
+  const user = request.user
   blog.user = user.id
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog.id)
@@ -25,16 +20,15 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  if (!request.user) {
     return response.status(401).json({ error: 'token invalid' })
   }
   const blog = await Blog.findById(request.params.id)
-  if (blog.user.toString() !== decodedToken.id) {
+  const user = request.user
+  if (blog.user.toString() !== user._id.toString()) {
     return response.status(401).json({ error: 'unauthorized action' })
   }
-  const user = await User.findById(blog.user.toString())
   user.blogs = user.blogs.filter((blog) => blog.toString() !== request.params.id)
   await user.save()
   await Blog.findByIdAndDelete(request.params.id)
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,7 +1,18 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
     request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    request.user = await User.findById(decodedToken.id)
   }
   next()
 }
@@ -25,6 +36,7 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   tokenExtractor,
+  userExtractor,
   unknownEndpoint,
   errorHandler
 }
