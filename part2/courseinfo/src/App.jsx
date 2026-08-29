27b31bca95e0e254a9146ab447d37133f1bbb0df
tapsaca <98756983+tapsaca@users.noreply.github.com>
2026-08-29 16:39:21 +0300

    Add solution to exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,38 +1,4 @@
-const Header = ({ text }) => {
-  return (
-    <h2>{text}</h2>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-    </>
-  )
-}
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>{part} {exercises}</p>
-  )
-}
-
-const Total = ({ parts }) => {
-  return (
-    <p><b>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header text={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
  const courses = [
