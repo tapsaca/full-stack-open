84461e4d977bd234349f75e73b960056c8b525f0
tapsaca <98756983+tapsaca@users.noreply.github.com>
2026-09-03 19:02:43 +0300

    Add solution to exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,21 +2,29 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [search, setNewSearch] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = { name: newName, number: newNumber }
-    if (persons.some(person => person.name === newPerson.name)) {
+    if (persons.some((person) => person.name === newPerson.name)) {
       alert(`${newPerson.name} is already added to phonebook`)
       return
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
     setNewNumber('')
+  }
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
   }
 
   const handleNameChange = (event) => {
@@ -30,6 +38,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,7 +55,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+        {persons
+          .filter((person) => person.name.toLowerCase().includes(search.toLowerCase()))
+          .map((person) => (
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div>
+          ))}
       </div>
     </div>
   )
