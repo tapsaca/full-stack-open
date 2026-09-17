3b7d0489c08cf33f2b5036fb4841f59ab46c5e87
tapsaca <98756983+tapsaca@users.noreply.github.com>
2026-09-17 22:10:23 +0300

    Add solution to exercise 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import personService from './services/persons'
 import Search from './components/Search'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [search, setNewSearch] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     personService
@@ -20,19 +22,37 @@ const App = () => {
     event.preventDefault()
     const personToUpdate = persons.find((person) => person.name === newName)
     if (personToUpdate) {
-      if (confirm(`${personToUpdate.name} is already added to phonebook, replace old number with a new one?`)) {
+      if (
+        confirm(
+          `${personToUpdate.name} is already added to phonebook, replace old number with a new one?`
+        )
+      ) {
         personService
-          .updatePerson(personToUpdate.id, { ...personToUpdate, number: newNumber })
-          .then((returnedPerson) => setPersons(persons.map((person) => person.id === returnedPerson.id ? returnedPerson : person)))
+          .updatePerson(personToUpdate.id, {
+            ...personToUpdate,
+            number: newNumber
+          })
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id === returnedPerson.id ? returnedPerson : person
+              )
+            )
+            setNotification(`Updated ${personToUpdate.name}`)
+          })
       } else {
         return
       }
     } else {
       const newPerson = { name: newName, number: newNumber }
-      personService
-        .createPerson(newPerson)
-        .then((returnedPerson) => setPersons(persons.concat(returnedPerson)))
+      personService.createPerson(newPerson).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson))
+        setNotification(`Added ${returnedPerson.name}`)
+      })
     }
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
     setNewName('')
     setNewNumber('')
   }
@@ -62,6 +82,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} />
       <Search value={search} onChange={handleSearchChange} />
       <h2>add a new</h2>
       <PersonForm
